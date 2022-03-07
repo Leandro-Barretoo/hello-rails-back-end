2a69884462c9a9d280e7196f1b562f45758d7e66
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-07 16:13:05 +0000

    Fix rubocop offenses

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,11 +1,11 @@
 class Api::GreetingsController < ApplicationController
-    def index
-        randgreeting = Greeting.order(Arel.sql('RANDOM()')).first
+  def index
+    randgreeting = Greeting.order(Arel.sql('RANDOM()')).first
 
-        render json: { greetings: [
-            {
-                message: randgreeting.message
-            }
-        ] }.to_json
-    end
+    render json: { greetings: [
+      {
+        message: randgreeting.message
+      }
+    ] }.to_json
+  end
 end
