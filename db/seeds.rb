70ed0f7ce6b94b05480aee25fb2e9f7697a55ad5
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-07 16:05:27 +0000

    Add model and populated with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: 'Nice to meet you!')
+Greeting.create(message: 'Good Afternoon.')
+Greeting.create(message: 'Long-time no see.')
+Greeting.create(message: 'Lovely to meet you!')
+Greeting.create(message: 'Hello stranger!')
