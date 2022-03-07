0faa87dd86df4b38ed30b1f80a5039e971f16787
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-07 16:12:09 +0000

    Add endpoint and retrive random message feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "api/greetings#index"
 end
